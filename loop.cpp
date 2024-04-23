f7130bb302f3857c444e5f0c6a7a0f086c0587d7
tsaiyichen <easonjessiejj@gmail.com>
2024-04-24 00:52:00 +0800

    0424 commit

---
+++ loop.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+void flush();
+int main(){
+    //while loop
+    int num = 0;
+    int total = 0;
+    while(num <= 100){
+        total += num;
+        num++;
+    }
+    cout << "using while loop: " << total << endl;
+
+    //for loop
+    total = 0;
+    for(int i = 0; i <= 100; i++){
+        total += i;
+    }
+    cout << "using for loop: " << total << endl;
+
+    //do while loop
+
+    num = 0;
+    total = 0;
+    do{
+        total += num;
+        num++;
+    }while(num <= 100);
+
+    cout << "using do whlie loop: " << total << endl;
+
+    int count;
+    cout << "Enter the end of the loop: ";
+    cin >> count;
+    cout << count << endl;
+    flush();
+    total = 0;
+    for(int i = count; i >= 0; i--){
+        total += i;
+    }
+    cout << "answer: " << total << endl;
+
+}
+void flush(){
+    while(cin.get() != '\n'){
+        continue;
+    }
+}
---
+++ selection.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void menu();
+void flush();
+int main(){
+    menu();
+    char ans;
+    cin >> ans;
+    flush();
+
+    //if-else
+    if(ans == 'a'){
+        cout << "you choose apple." << endl;
+    }else if(ans == 'b'){
+        cout << "you choose banana." << endl;
+    }else if(ans == 'c'){
+        cout << "you choose cat." << endl;
+    }else if(ans == 'd'){
+        cout << "you choose dog." << endl;
+    }else{
+        cout << "wrong!" << endl;
+    }
+
+    //switch case
+    switch(ans){
+        case 'a':
+            cout << "you choose apple." << endl;
+            break;
+        case 'b':
+            cout << "you choose banana." << endl;
+            break;
+        case 'c':
+            cout << "you choose cat." << endl;
+            break;
+        case 'd':
+            cout << "you choose dog." << endl;
+            break;
+        default:
+            cout << "wrong!" << endl;
+    }
+
+    //?:
+    string printout = (ans ==  'a') ? "you choose apple." : (ans == 'b') ? "you choose banana." : (ans == 'c') ? "you choose cat." : (ans == 'd') ? "you choose dog." : "wrong";
+    cout << printout << endl;
+}
+
+void menu(){
+    cout << "choose: (a)apple, (b)banana, (c)cat, (d)dog: ";
+}
+void flush(){
+    while(cin.get() != '\n'){
+        continue;
+    }
+}
